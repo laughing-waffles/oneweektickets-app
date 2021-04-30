4743ddc69a34d941a29d1065541224001991c221
Ed Ireson <ed@ireson.net>
2021-04-30 12:18:27 -0500

    Update fomo.js

--- assets/js/fomo.js
+++ assets/js/fomo.js
@@ -25,24 +25,26 @@ function doBounce(element, times, distance, speed) {
             .animate({marginLeft: '+='+distance}, speed);
     }        
 }
+function runMath() {
 
-$( "#qty" ).change(function() {
 	$('#qty').val();
 	$("#listqty").text($('#qty').val());
 	$('#listqty').data("raw",$('#qty').val());
+
+$("#listbid").text(currency($('#amt').val()).format());
+$('#listbid').data("raw",currency($('#amt').val()));
+$("#listsub").text(currency($('#amt').val() * $('#qty').val()).format());
+	$("#listsub").data("raw",currency($('#amt').val() * $('#qty').val()).format());
+$("#listfee").text(currency($('#amt').val()).multiply($('#qty').val()).multiply(.07).format());
+$("#listfee").data("raw",currency($('#amt').val()).multiply($('#qty').val()).multiply(.07));
+$("#listtotal").text(currency($('#listfee').data("raw")).add($('#listsub').data("raw")).format());
+}
+$( "#qty" ).change(function() {
+	runMath();
 });
 $( "#amt" ).keyup(function() {
 	if ($('#amt').val() !== "") {
-
-	
-	
-	$("#listbid").text(currency($('#amt').val()).format());
-	$('#listbid').data("raw",currency($('#amt').val()));
-	$("#listsub").text(currency($('#amt').val() * $('#qty').val()).format());
-		$("#listsub").data("raw",currency($('#amt').val() * $('#qty').val()).format());
-	$("#listfee").text(currency($('#amt').val()).multiply($('#qty').val()).multiply(.07).format());
-	$("#listfee").data("raw",currency($('#amt').val()).multiply($('#qty').val()).multiply(.07));
-	$("#listtotal").text(currency($('#listfee').data("raw")).add($('#listsub').data("raw")).format());
+	runMath();
 	}
 	else {
 
